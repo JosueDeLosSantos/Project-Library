e11b5436ec1af44749e2b11014726ff134e2995a
Josue <jdd1993@hotmail.com>
2022-11-27 22:32:26 -0400

    Experiment

--- script.js
+++ script.js
@@ -1,69 +1,33 @@
 
 
-
-
-let myLibrary = [];
-
-/*This function converts the information provided into an array and then converts the array
-into an object*/
-addBookToLibrary.prototype.printer = function(){
-    
-    //converts info into aray
-    let temp = [this.title, this.pages]
-    //converts array into object
-    let obj = {...temp}
-    return obj
-
-}
-
-//Sets the constructor prototype to inherit functions from it
-Book.prototype = Object.create(addBookToLibrary.prototype)
-
-//constructor
-function Book(title, pages) {
-    this.title = title,
-    this.pages = pages
-  // the constructor...
-}
-
-let example1 = new Book('my fabe', 100)
-
-//Adds object into the myLibrary array
-function addBookToLibrary() {
-
-    myLibrary.push(example1.printer())
-
-  // do stuff here
-}
-
-addBookToLibrary()
-
-console.log(myLibrary)
-
-/***Data collection****/
-
 let body = document.querySelector('body')
 let form = document.querySelector('form')
 let btn = document.querySelector('#btn')
 let namebox = document.querySelector('#name')
 
 
-let myNewArr = []
 
+let myLibrary = [];
 
+function Book() {
+  // the constructor...
+}
 
-function goNow(e){
-    myNewArr.push(namebox.value)
+function addBookToLibrary(e) {
+  // do stuff here
+
+  let temp = {name: `${namebox.value}`}
+  
+    myLibrary.push(temp)
+    console.log(myLibrary)
     //clears input
     namebox.value = '';
-    console.log(myNewArr)
     //prevents button from refreshing the page
     e.preventDefault();
 }
 
 
 
-
-btn.addEventListener('click', goNow)
+btn.addEventListener('click', addBookToLibrary)
 
 

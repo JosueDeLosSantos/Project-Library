9749bb8e4b02f2ae835f00941ffd255b2e5f8587
Josue <jdd1993@hotmail.com>
2022-11-27 21:04:35 -0400

    Experiment

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 
 
+
+
 let myLibrary = [];
 
+/*This function converts the information provided into an array and then converts the array
+into an object*/
 addBookToLibrary.prototype.printer = function(){
-
+    
+    //converts info into aray
     let temp = [this.title, this.pages]
+    //converts array into object
     let obj = {...temp}
     return obj
 
 }
 
+//Sets the constructor prototype to inherit functions from it
 Book.prototype = Object.create(addBookToLibrary.prototype)
 
+//constructor
 function Book(title, pages) {
     this.title = title,
     this.pages = pages
@@ -20,6 +28,7 @@ function Book(title, pages) {
 
 let example1 = new Book('my fabe', 100)
 
+//Adds object into the myLibrary array
 function addBookToLibrary() {
 
     myLibrary.push(example1.printer())
@@ -33,16 +42,28 @@ console.log(myLibrary)
 
 /***Data collection****/
 
+let body = document.querySelector('body')
+let form = document.querySelector('form')
 let btn = document.querySelector('#btn')
-let box = document.querySelector('#name')
+let namebox = document.querySelector('#name')
 
 
-function goNow(){
-    console.log(box.value)
+let myNewArr = []
+
+
+
+function goNow(e){
+    myNewArr.push(namebox.value)
+    //clears input
+    namebox.value = '';
+    console.log(myNewArr)
+    //prevents button from refreshing the page
+    e.preventDefault();
 }
 
 
 
 
 btn.addEventListener('click', goNow)
+
 

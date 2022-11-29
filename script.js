fb76ebb6b384e1101c0cdbe3a02c953764ff2f68
Josue <jdd1993@hotmail.com>
2022-11-29 14:11:43 -0400

    Experiment

--- script.js
+++ script.js
@@ -36,7 +36,39 @@ function addBookToLibrary(e) {
   let temp = new Book(titlebox.value, authorbox.value, pagesbox.value, readbox.value)
   
     myLibrary.push(temp)
-    console.log(myLibrary)
+    //////////////////////////////////////////////////
+    for (let i in myLibrary){
+      if(i in myLibrary){
+    
+        //creates a card on every cycle
+        const card = document.createElement('div')
+        card.classList.add('card')
+        cardList.append (card)
+    
+        for (let key in temp) {
+          
+          if (key in temp) {
+            //creates a div on every cycle
+            const category = document.createElement('div')
+            category.classList.add('category')
+            const span = document.createElement('span')
+            span.classList.add('header')
+            const span2 = document.createElement('span')
+            span2.classList.add('response')
+            card.append(category)
+            category.append(span)
+            category.append(span2)
+            //MAGIC
+            span.innerHTML += key+": ";
+            span2.innerHTML += temp[key];
+          }
+        } 
+    
+      }
+    }
+
+
+    //////////////////////////////////////////////////
     //clears input
     titlebox.value = '';
     authorbox.value = '';
@@ -60,62 +92,10 @@ function adder(){
 btn.addEventListener('click', addBookToLibrary)
 add.addEventListener('click', adder)
 
-/*
-let arr1 = [{name: 'b'},{author: 'a'}]
-
-for(let i = 0; i < arr1.length; i++) {
-  console.log(arr1[i])
-}*/
 
 
-//EXPERIMENT
-let mine = new Book('yours', 'josue', 900, false)
 
-let checker = []
-checker.push(mine)
-checker.push(mine)
-checker.push(mine)
-checker.push(mine)
-checker.push(mine)
-checker.push(mine)
-checker.push(mine)
-checker.push(mine)
-checker.push(mine)
-checker.push(mine)
-checker.push(mine)
-checker.push(mine)
 
-console.log(checker)
-
-for (let i in checker){
-  if(i in checker){
-
-    //creates a card on every cycle
-    const card = document.createElement('div')
-    card.classList.add('card')
-    cardList.append (card)
-
-    for (let key in mine) {
-      //creates a div on every cycle
-      if (key in mine) {
-
-        const category = document.createElement('div')
-        category.classList.add('category')
-        const span = document.createElement('span')
-        span.classList.add('header')
-        const span2 = document.createElement('span')
-        span2.classList.add('header2')
-        category.append(span)
-        category.append(span2)
-        card.append(category)
-        //MAGIC
-        span.innerHTML += key+": ";
-        span2.innerHTML += mine[key];
-      }
-    } 
-
-  }
-}
 
 
 /* 
@@ -126,18 +106,5 @@ for (let i in ObjectArray){
     //this is used to loop through arrays and objects.
   }
 }
-
-} */
-
-
-
-for (let i in mine) {
-  //creates a div on every cycle
-  if (i in mine) {
-
-    console.log(i)
-    
-  }
-} 
-
+*/
 

3f9e465d97865295bb766a82151db44a105a701b
Josue <jdd1993@hotmail.com>
2022-11-28 20:16:22 -0400

    Experiment

--- script.js
+++ script.js
@@ -3,41 +3,52 @@
 let body = document.querySelector('body')
 let form = document.querySelector('form')
 let btn = document.querySelector('#btn')
-let namebox = document.querySelector('#name')
+let titlebox = document.querySelector('#title')
+let authorbox = document.querySelector('#author')
+let pagesbox = document.querySelector('#pages')
+let readbox = document.querySelector('#read')
 let add = document.querySelector('#add')
+let cardList = document.querySelector('.card-list')
 
 //Hides the 'add' button when the page first load.
-add.hidden = true
+form.hidden = true
 
 
 let myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
   // the constructor...
+  this.title = title,
+  this.author = author,
+  this.pages = pages,
+  this.read = read
 }
 
 function addBookToLibrary(e) {
   // do stuff here
 
-  add.hidden = false
-
-  let temp = {name: `${namebox.value}`}
+  let temp = new Book(titlebox.value, authorbox.value, pagesbox.value, readbox.value)
   
     myLibrary.push(temp)
     console.log(myLibrary)
     //clears input
-    namebox.value = '';
+    titlebox.value = '';
+    authorbox.value = '';
+    pagesbox.value = '';
+    readbox.value = '';
     //prevents form buttons from refreshing the page
     e.preventDefault();
 
     form.hidden = true
     add.hidden = false
+    cardList.hidden = false
 }
 
 function adder(){
 
     form.hidden = false
     add.hidden = true
+    cardList.hidden = true
 }
 
 btn.addEventListener('click', addBookToLibrary)

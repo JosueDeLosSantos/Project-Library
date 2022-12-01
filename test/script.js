8d08ad5fd8a2c87e2373e10a3a8ae9e756bc61f8
Josue <jdd1993@hotmail.com>
2022-12-01 00:37:52 -0400

    Experiment

--- test/script.js
+++ test/script.js
@@ -1,14 +1,26 @@
 
 
-let myArr = ['apple', 'grape', 'lemon']
 
 
+//Selects all boxes
+const box = document.querySelectorAll('.box')
+//Converts all boxes into an array
+const boxArr = Array.from(box)
 
-/* removes specific array elements. first number
-indicates position, the secon one indicates the amount of items to be remove */
-myArr.splice(1, 1) 
+function deleter(e){
+    console.log(e)
+    console.log(e.target)
+   //Targets the 'clicked' node
+   const found = e.target
+   
+   for (let i in boxArr){
+    
+    if(boxArr[i] == found)
+    found.dataset.test = `${i}`
+   }
+    
+}
 
-console.log(myArr)
-
-
-
+box.forEach(box => {
+    box.addEventListener('click', deleter)
+})
